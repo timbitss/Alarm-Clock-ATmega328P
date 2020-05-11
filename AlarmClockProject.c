0751878b69e1722c6ed0e584fdcf6d9fa30a38cd
Timothy <Timothy@DESKTOP-PJVQ42V.local>
2020-05-10 19:11:40 -0700

    Update AlarmClockProject.c, AlarmClockProject.elf, and 5 more files...

--- AlarmClockProject.c
+++ AlarmClockProject.c
@@ -13,21 +13,28 @@
 #include "timers.h"
 #include "interrupts.h"
 
-#define intPerMin 230 //number of interrupts per minute
+#define intPerSec 61 //number of interrupts per second with prescaler of 64
+
 
 volatile char secondtick = 0; 
-volatile char minutes = 0;
+volatile unsigned int seconds = 0;
+volatile unsigned int minutes = 0;
 
 ISR(TIMER0_OVF_vect){
     secondtick++;
-    if(secondtick == intPerMin){
+    if(secondtick == intPerSec){
         secondtick = 0;
+        seconds++;
+    }
+    if(seconds == 60){
+        seconds = 0;
         minutes++;
     }
 }
 
 int main(void){
 
+   char buffer[2];
    initializeSecondsTimer();
    initializeSecondsInterrupt();
    lcd_init(LCD_DISP_ON);
@@ -35,9 +42,17 @@ int main(void){
    sei(); //enable global interrupts
 
    while(1){ //loops forever
+        
+    lcd_gotoxy(1,0);
+    itoa( seconds % 10, buffer, 10); //puts first digit in buffer
+    lcd_puts(buffer); //displays first digit
+    lcd_gotoxy(0,0);
+    itoa( seconds / 10, buffer, 10); //puts second digit in buffer
+    lcd_puts(buffer); //displays second digit
     
-    lcd_putc('a'); //display minutes elapsed
+  
     
+   
 
    }
 
--- timers.c
+++ timers.c
@@ -4,7 +4,7 @@
 void initializeSecondsTimer(void){
     
     
-    TCCR0B |= ((1 << CS02) | (1 << CS00)); //initializing Timer 0 to internal CPU clk (1Mhz) / 1024
+    TCCR0B |= ((1 << CS01) | (1 << CS00)); //initializing Timer 0 to internal CPU clk (1Mhz) / 64
 
 
 }
